274039ec7ed0fedca2e20ec0c1ac6b86640cf49f
Juan Carlos Medina <jcmedinamartinez@gmail.com>
2016-05-22 02:56:58 -0500

    cambio

--- js/script.js
+++ js/script.js
@@ -1,46 +1,46 @@
-$(document).ready(function(){
-  var ua = navigator.userAgent.toLowerCase(),
+var ua = navigator.userAgent.toLowerCase(),
   platform = navigator.platform.toLowerCase();
-  platformName = ua.match(/ip(?:ad|od|hone)/) ? 'ios' : (ua.match(/(?:webos|android)/) || platform.match(/mac|win|linux/) || ['other'])[0],
-  isMobile = /ios|android|webos/.test(platformName);
-  if (!isMobile) {
+platformName = ua.match( /ip(?:ad|od|hone)/ ) ? 'ios' : ( ua.match( /(?:webos|android)/ ) || platform.match( /mac|win|linux/ ) || [ 'other' ] )[ 0 ],
+  isMobile = /ios|android|webos/.test( platformName );
+if ( isMobile ) {
+  console.log('movil');
+  $zopim(function() {
+    $zopim.livechat.badge.hide();
     $zopim.livechat.button.hide();
-    console.log('movil');
-  }
-})
-
-
-var vid = document.getElementById("bgvid");
-var pauseButton = document.querySelector("button");
-
-function vidFade() {
-  vid.classList.add("stopfade");
+  });
 }
 
-vid.addEventListener('ended', function()
-{
-// only functional if "loop" is removed
-vid.pause();
-// to capture IE10
-vidFade();
-});
+
+var vid = document.getElementById( "bgvid" );
+var pauseButton = document.querySelector( "button" );
+
+function vidFade() {
+  vid.classList.add( "stopfade" );
+}
+
+vid.addEventListener( 'ended', function () {
+  // only functional if "loop" is removed
+  vid.pause();
+  // to capture IE10
+  vidFade();
+} );
 
 
 /*pauseButton.addEventListener("click", function() {
-  vid.classList.toggle("stopfade");
-  if (vid.paused) {
-    vid.play();
-    pauseButton.innerHTML = "Pause";
-  } else {
-    vid.pause();
-    pauseButton.innerHTML = "Paused";
-  }
+vid.classList.toggle("stopfade");
+if (vid.paused) {
+  vid.play();
+  pauseButton.innerHTML = "Pause";
+} else {
+  vid.pause();
+  pauseButton.innerHTML = "Paused";
+}
 })*/
 
 /*$zopim( function() {
-  window.setTimeout( function() {
-    $zopim.livechat.window.show();
-  }, 10000 ); //time in milliseconds
+window.setTimeout( function() {
+  $zopim.livechat.window.show();
+}, 10000 ); //time in milliseconds
 
 } );*/
 
@@ -48,6 +48,6 @@ vidFade();
 //    $zopim.livechat.button.hide();
 //  });
 
-$('#zopimPrompt').click(function(){
+$( '#zopimPrompt' ).click( function () {
   $zopim.livechat.window.show();
-})
+} )
